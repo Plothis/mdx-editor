7bb1af5d96606f9d528a6b7b2d72793548fef32e
hb-bobo <751968840@qq.com>
2021-09-26 03:31:49 +0800

    fix: api error

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -2,7 +2,7 @@ import { Object2FormData } from '../utils';
 import request, { checkResult } from '../utils/request';
 
 
-const prefix2 = 'http://114.67.103.78:3003//api';
+const prefix2 = 'http://114.67.103.78:3003/api';
 export function uploadImg(data: any) {
   return checkResult(request({
     method: "POST",
