6e7c616268eb3913b6f6f479de1923ce5310a806
hb-bobo <751968840@qq.com>
2021-09-26 02:11:16 +0800

    feat: api prefix

--- src/api/upload.ts
+++ src/api/upload.ts
@@ -1,10 +1,8 @@
 import { Object2FormData } from '../utils';
 import request, { checkResult } from '../utils/request';
 
-const prefix = `/v2`
 
-const smURL = 'https://sm.ms/api/v2';
-const prefix2 = 'http://localhost:3003/api';
+const prefix2 = 'http://114.67.103.78:3003//api';
 export function uploadImg(data: any) {
   return checkResult(request({
     method: "POST",
